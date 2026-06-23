1bbfc85c1db59079677b8447d0ae2a220f7cb552
axlrose-young <arjkris789@gmail.com>
2026-06-23 10:53:03 +0000

    went through whole code

--- intel.c
+++ intel.c
@@ -1,3 +1,5 @@
+// work on push stack implement get register pair commands
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -52,59 +54,6 @@ void chip_init(chip* c)
 }
 
 // Helper functions
-
-void rp_push(uint8_t opcode, chip* c)
-{
-	uint8_t rp = (opcode >> 4) & 3;
-	switch(rp)
-	{	
-		case 0: 
-			c->sp--;
-			c->memory[c->sp] = c->reg[0];
-			c->sp--;
-			c->memory[c->sp] = c->reg[1];
-			break;
-		case 1:  
-			c->sp--;
-			c->memory[c->sp] = c->reg[2];
-			c->sp--;
-			c->memory[c->sp] = c->reg[3];
-			break;
-		case 2: 
-			c->sp--;
-			c->memory[c->sp] = c->reg[4];
-			c->sp--;
-			c->memory[c->sp] = c->reg[5];
-			break;
-	}
-}
-
-void rp_pop(uint8_t opcode, chip* c)
-{
-	uint8_t rp = (opcode >> 4) & 3;
-	switch(rp)
-	{	
-		case 0: 
-			c->reg[1] = c->memory[c->sp];	 // resgiter C
-			c->sp++;
-			c->reg[0] = c->memory[c->sp];	 // resgiter B
-			c->sp++;			
-			break;
-		case 1:  
-			c->reg[3] = c->memory[c->sp];	 // register E 
-			c->sp++;
-			c->reg[2] = c->memory[c->sp];	 // register D
-			c->sp++;
-			break;
-		case 2: 
-			c->reg[5] = c->memory[c->sp];	 // register L 
-			c->sp++;
-			c->reg[4] = c->memory[c->sp];	 // register H
-			c->sp++;
-			break;
-	}
-}
-
 uint16_t make_addr(chip* c)
 {
 	return c->memory[c->pc + 2] << 8 | c->memory[c->pc + 1];
@@ -160,6 +109,20 @@ void handle_zsp(uint8_t result, chip* c){
 	handle_pf(result, c);
 }
 
+// Getting resgister pairs
+
+uint16_t get_bc(chip* c){
+	return (c->b<<8) | c->c;
+}
+
+uint16_t get_de(chip* c){
+	return (c->d<<8) | c->e;
+}
+
+uint16_t get_hl(chip* c){
+	return (c->h<<8) | c->l;
+}
+
 // Instruction families
 void ora(uint8_t val, chip* c){
 	c->a |= val;
@@ -196,67 +159,6 @@ uint16_t pop_stack(chip* c){
 	return (high << 8) | low;
 }
 
-int increment(uint8_t opcode, chip* c)
-{
-	uint8_t index = (opcode >> 3) & 7; 
-	if(index != 6)
-	{
-		c->reg[index]++;	
-		handle_zf(c->reg[index],c);
-		handle_sf(c->reg[index],c);
-		handle_pf(c->reg[index],c);
-		handle_ac_add(c->reg[index],0x01,c);
-		c->pc+=1;
-		return 5;
-	}
-	else 
-	{
-		uint16_t addr = (c->reg[4] << 8) | c->reg[5];	
-		c->memory[addr]++;
-		handle_zf(c->memory[addr],c);
-		handle_sf(c->memory[addr],c);
-		handle_pf(c->memory[addr],c);
-		handle_ac_add(c->memory[addr], 0x01,c);
-		c->pc+=1;
-		return 10;
-	}
-}
-
-void increment_rp(uint8_t opcode, chip* c)
-{
-	uint8_t rp = (opcode >> 4) & 0xff;
-	switch(rp)
-	{
-		case 0: 
-			{
-				uint16_t val = (c->reg[0]<<8) | c->reg[1]; 
-				val++;
-				c->reg[0] = val >> 8;
-				c->reg[1] = val & 0xff;
-				break;
-			}
-		case 1: 
-			{
-				uint16_t val = (c->reg[2]<<8) | c->reg[3]; 
-				val++;
-				c->reg[2] = val >> 8;
-				c->reg[3] = val & 0xff;
-				break;
-			}
-		case 2: 
-			{
-				uint16_t val = (c->reg[4]<<8) | c->reg[5]; 
-				val++;
-				c->reg[4] = val >> 8;
-				c->reg[5] = val & 0xff;
-				break;	
-			}
-		case 3: 
-			c->sp++; 
-			break;
-	}
-}
-
 uint8_t dcr(uint8_t val, chip* c){
 	c->ac = (val&0x0f) >= (0x01);
 	val--;
@@ -267,6 +169,20 @@ uint8_t dcr(uint8_t val, chip* c){
 void dcr_pair(uint8_t* high, uint8_t* low, chip* c){
 	uint16_t val = (*high << 8) | *low;
 	val--;
+	*high = val >> 8;	
+	*low = val & 0xff;
+}
+
+uint8_t inr(uint8_t val, chip* c){
+	c->ac = ((val&0x0f) + 0x01) > 0x0f; 
+	val++;
+	handle_zsp(val,c);
+	return val;
+}
+
+void inr_pair(uint8_t* high, uint8_t* low, chip* c){
+	uint16_t val = (*high << 8) | *low;
+	val++;
 	*high = val >> 8;	
 	*low = val & 0xff;
 }
@@ -301,24 +217,12 @@ void cmp(uint8_t val, chip* c){
 	handle_zsp(result,c);
 }
 
-void dad(uint8_t opcode, chip* c)
-{
-	uint8_t rp = (opcode >> 4) & 3;
-	uint16_t hl_rp = (c->reg[4] << 8) | c->reg[5];
-	uint16_t value2;
-	switch(rp)
-	{
-		case 0: value2 = (c->reg[0] << 8) | c->reg[1]; break;
-		case 1: value2 = (c->reg[2] << 8) | c->reg[3]; break;
-		case 2: value2 = (c->reg[4] << 8) | c->reg[5]; break;
-		case 3: value2 = c->sp; break;
-	}
-	uint16_t result = hl_rp + value2; 	// if result overflow it wraps around to make a smaller value
-	if(result < hl_rp) { c->cy = 1; }
-	else { c->cy = 0; }
-	
-	c->reg[4] = (result >> 8);
-	c->reg[5] = (result & 0xff);
+void dad(uint16_t val, chip* c){
+	uint16_t hl = get_hl(c);
+	hl += val;
+	c->cy = (hl < val);			// result if overflows wraps around
+	c->h = hl >> 8;
+	c->l = hl & 0xff;
 }
 
 void debug(uint8_t opcode,chip* c);
@@ -361,13 +265,13 @@ int execute(chip* c)
 		case 0x47: c->b = c->a; c->pc++; break;
 		case 0x69: c->l = c->c; c->pc++; break;
 
-		case 0x7e: c->a = c->memory[(c->h << 8) | c->l]; c->pc++; break; 
-		case 0x66: c->h = c->memory[(c->h << 8) | c->l]; c->pc++; break; 
-		case 0x5e: c->e = c->memory[(c->h << 8) | c->l]; c->pc++; break;
- 		case 0x46: c->b = c->memory[(c->h << 8) | c->l]; c->pc++; break;
- 		case 0x4e: c->c = c->memory[(c->h << 8) | c->l]; c->pc++; break;
+		case 0x7e: c->a = c->memory[get_hl(c)]; c->pc++; break; 
+		case 0x66: c->h = c->memory[get_hl(c)]; c->pc++; break; 
+		case 0x5e: c->e = c->memory[get_hl(c)]; c->pc++; break;
+ 		case 0x46: c->b = c->memory[get_hl(c)]; c->pc++; break;
+ 		case 0x4e: c->c = c->memory[get_hl(c)]; c->pc++; break;
 
-		case 0x77: c->memory[(c->h << 8) | c->l] = c->a; c->pc++; break;
+		case 0x77: c->memory[get_hl(c)] = c->a; c->pc++; break;
 
 		// MVI commands
 		case 0x3e: c->a = c->memory[c->pc+1]; c->pc+=2; break;
@@ -375,16 +279,16 @@ int execute(chip* c)
 		case 0x26: c->h = c->memory[c->pc+1]; c->pc+=2; break;
 		case 0x0e: c->c = c->memory[c->pc+1]; c->pc+=2; break;
 		case 0x16: c->d = c->memory[c->pc+1]; c->pc+=2; break;
-		case 0x36: c->memory[(c->h << 8) | c->l] = c->memory[c->pc+1]; 
+		case 0x36: c->memory[get_hl(c)] = c->memory[c->pc+1]; 
 			   c->pc+=2;
 			   break;
 
 		// ORA commands
-		case 0xb6: ora(c->memory[(c->h<<8)|c->l],c); c->pc++; break;
+		case 0xb6: ora(c->memory[get_hl(c)],c); c->pc++; break;
 		case 0xb1: ora(c->c,c); c->pc++; break;
 
 		// XRA commands
-		case 0xae: xra(c->memory[(c->h<<8)|c->l],c); c->pc++; break;
+		case 0xae: xra(c->memory[get_hl(c)],c); c->pc++; break;
 		case 0xa8: xra(c->b,c); c->pc++; break;
 		case 0xa9: xra(c->c,c); c->pc++; break;
 		
@@ -398,9 +302,9 @@ int execute(chip* c)
 		case 0x3a: c->a = c->memory[make_addr(c)]; c->pc+=3; break; 		// lda addr
 		case 0x32: c->memory[make_addr(c)] = c->a; c->pc+=3; break;		// sta addr
 										
-		case 0x12: c->memory[(c->d<<8) | c->e] = c->a; c->pc++; break;		// stax
+		case 0x12: c->memory[get_de(c)] = c->a; c->pc++; break;			// stax d,e
 
-		case 0x1a: c->a = c->memory[(c->d<<8) | c->e]; c->pc++; break;		// ldax
+		case 0x1a: c->a = c->memory[get_de(c)]; c->pc++; break;			// ldax d,e
 
 		case 0x2a:								// lhld 
 			c->l = c->memory[make_addr(c)];		
@@ -412,7 +316,7 @@ int execute(chip* c)
 			c->memory[make_addr(c)+1] = c->h;
 			c->pc+=3; break;
 
-		case 0xf9: c->sp = (c->h<<8)|c->l; c->pc++; break;			// sphl
+		case 0xf9: c->sp = get_hl(c); c->pc++; break;				// sphl
 
 		// cmp commands
 		case 0xfe: cmp(c->memory[c->pc+1], c); c->pc+=2; break;			// cpi
@@ -534,14 +438,6 @@ int execute(chip* c)
 		case 0xa1: logical_and(c->c,c); c->pc++; break;			// ana c
 		case 0xa0: logical_and(c->b,c); c->pc++; break;			// ana b
 	
-		// rotate
-		case 0x0f:							// rrc
-			c->cy = c->a & 1;
-			c->a >>= 1;
-			c->a |= (c->cy << 7);	
-			c->pc++;
-			break; 
-
 		// DCR commands
 		case 0x05: c->b = dcr(c->b,c); c->pc++; break;			// dcr b
 		case 0x0d: c->d = dcr(c->d,c); c->pc++; break;			// dcr c
@@ -551,46 +447,51 @@ int execute(chip* c)
 		case 0x0b: dcr_pair(&c->b,&c->c,c); c->pc++; break;		// dcx b
 									
 		// INR commands
-		case 0x3c: return increment(opcode,c);
-		case 0x14: return increment(opcode,c);
-		case 0x34: return increment(opcode,c);
-
-		
+		case 0x3c: c->a = inr(c->a,c); c->pc++; break;			// inr a
+		case 0x14: c->d = inr(c->d,c); c->pc++; break;			// inr d
+		case 0x34: 							// inr m
+			   uint8_t data = c->memory[get_hl(c)];
+			   data = inr(data,c); c->pc++; break;
 		// INX commands
-		case 0x23: increment_rp(opcode,c); c->pc+=1; return 5;
-		case 0x13: increment_rp(opcode,c); c->pc+=1; return 5;
+		case 0x23: inr_pair(&c->h,&c->l,c); c->pc++; break;		// inx h,l 
+		case 0x13: inr_pair(&c->d,&c->e,c); c->pc++; break;		// inx d,e
 						
-		case 0x09: dad(opcode,c); c->pc+=1; return 10;
-		case 0x19: dad(opcode,c); c->pc+=1; return 10;
-		case 0x29: dad(opcode,c); c->pc+=1; return 10;
-		case 0x39: dad(opcode,c); c->pc+=1; return 10;
-		case 0xeb: 
-			   uint16_t hl_rp = (c->reg[4] << 8) | c->reg[5];
-			   uint16_t de_rp = (c->reg[2] << 8) | c->reg[3];
-			   uint16_t copy = hl_rp;
-			   hl_rp = de_rp;
-			   de_rp = copy;
-			   c->reg[4] = hl_rp >> 8;
-			   c->reg[5] = hl_rp & 0xff;
-			   c->reg[2] = de_rp >> 8;
-			   c->reg[3] = de_rp & 0xff;
+		case 0x09: dad(get_bc(c),c); c->pc++; break; 			// dad b,c
+		case 0x19: dad(get_de(c),c); c->pc++; break;			// dad d,e
+		case 0x29: dad(get_hl(c),c); c->pc++; break;			// dad h,l
+		case 0x39: dad(c->sp,c); c->pc++; break;			// dad sp
 
-			   c->pc+=1;
-			   return 4;
+		case 0xeb:							// xchg h,l with d,e 
+			   uint16_t hl = get_hl(c);
+			   uint16_t de = get_de(c);
+			   uint16_t tmp = hl;
+			   hl = de; de = tmp;
+			   c->h = hl >> 8; c->l = hl & 0xff;
+			   c->d = de >> 8; c->e = de & 0xff;
+			   c->pc++; break;
 
-		case 0x07:
-			c->cy = (c->reg[7] >> 7);
-			c->reg[7] <<= 1;
-			c->reg[7] |= c->cy;
+		// rotate
+		case 0x0f:							// rrc
+			c->cy = c->a & 1;
+			c->a >>= 1;
+			c->a |= (c->cy << 7);	
+			c->pc++;
+			break; 
+		case 0x07:							// rlc
+			c->cy = (c->a >> 7);
+			c->a <<= 1;
+			c->a |= c->cy;
+			c->pc++;
+			break;
+		
+		// interupts
+		case 0xf3: c->INTE = 0; c->pc++; break;				// di
+		case 0xfb: c->INTE = 1; c->pc++; break;				// ei
 
-			c->pc+=1;
-			return 4;
-
-		case 0xf3: c->INTE = 0; c->pc+=1; return 4;
-		case 0xfb: c->INTE = 1; c->pc+=1; return 4;
-
-		case 0x00: c->pc+=1; return 4;
-		case 0x37: c->cy = 1; c->pc+=1; return 4;
+		case 0x00: c->pc++; break;					// nop
+										//
+		case 0x37: c->cy = 1; c->pc++; break;				// stc
+										//
 		default: 	
 			printf("Unimplemented: %x\n",opcode);
 			is_running = 0;
@@ -618,34 +519,31 @@ int main()
 void debug(uint8_t opcode,chip* c)
 {
 	fprintf(stderr,"Opcode: %x, PC: %x\n",opcode, c->pc);	
-	fprintf(stderr,"Registers  B: %x C: %x D: %x E: %x H: %x L: %x M: %x A: %x\n",
-			c->reg[0],c->reg[1],c->reg[2],c->reg[3],
-		       c->reg[4], c->reg[5],c->reg[6],c->reg[7]);	
+	fprintf(stderr,"Registers  B: %x C: %x D: %x E: %x H: %x L: %x A: %x\n",
+			c->b,c->c,c->d,c->e,
+		       c->h, c->l,c->a);	
 	fprintf(stderr,"Flags  Z: %d S: %d P: %d CY: %d AC: %d Stack Ptr: %x\n",c->zf,c->sf,c->pf,c->cy,c->ac,c->sp);
 	fprintf(stderr,"\t---\n");
 }
 
 void mini_bdos(chip* c)
 {
-	switch(c->reg[1])
+	switch(c->c)
 	{
 		case 9:
-			c->pc = (c->reg[2] << 8) | c->reg[3];	
+			c->pc = get_de(c);	
 			while(c->memory[c->pc] != '$')
 			{
 				printf("%c",c->memory[c->pc]);	
-				c->pc+=1;
+				c->pc++;
 			}
 			fflush(stdout);
+
 			// RET to addr after CALL 5
-			uint8_t low = c->memory[c->sp]; 			
-			c->sp++;
-			uint8_t high = c->memory[c->sp];
-			c->sp++;
-			c->pc = (high << 8) | low;
+			c->pc = pop_stack(c);
 			break;
 		default:
-			printf("unimplemented bdos: %d\n",c->reg[1]);
+			printf("unimplemented bdos: %d\n",c->c);
 			break;
 	}
 }
